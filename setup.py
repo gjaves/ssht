1bc321d3b1bd0a3f7ec24f5bcb640865b53defd2
Jan (javes) Veselý <gjaves@gmail.com>
2019-09-04 22:14:33 +0200

    Add setup.py...

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!python
+
+from setuptools import setup
+
+setup(name='ssht',
+      version='0.1',
+      description='Always use tmux with ssh.',
+      #url='http://github.com/storborg/funniest',
+      author='Jan (javes) Veselý',
+      author_email='gjaves@gmail.com',
+      license='MIT',
+      scripts=['bin/ssht'],
+      #packages=['funniest'],
+      #zip_safe=False
+      )
+
